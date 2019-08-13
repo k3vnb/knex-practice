27f67835638f816aaface9c947db2836a893c76f
Kevin Boyle <papershack@gmail.com>
2019-08-12 22:19:17 -0700

    add drills for pagination, date-based query, and summing total in column

--- src/drills.js
+++ src/drills.js
@@ -16,4 +16,45 @@ function findProductByKeyword(keyword){
         })
 }
 
-findProductByKeyword('Pretend')
+function getProductsByDateAfter(daysAgo){
+    knexInstance
+        .select('name', 'date_added')
+        .from('shopping_list')
+        .where(
+            'date_added',
+            '>',
+            knexInstance.raw(`now() - '?? DAYS'::INTERVAL`, daysAgo)
+        )
+        .then(res => {
+            console.log(res)
+        })
+}
+
+function paginateShoppingListItems(pageNumber){
+    const productsPerPage = 6
+    const offset = productsPerPage * (pageNumber - 1)
+    knexInstance
+        .select('name', 'category', 'price')
+        .from('shopping_list')
+        .limit(6)
+        .offset(offset)
+        .then(res => {
+            console.log(res)
+        })
+}
+
+function getTotalCostPerCategory(){
+    knexInstance
+        .select('category')
+        .sum('price')
+        .from('shopping_list')
+        .groupBy('category')
+        .then(res => {
+            console.log(res)
+        })
+}
+
+// getTotalCostPerCategory()
+paginateShoppingListItems(15)
+// findProductByKeyword('pretend')
+// getProductsByDateAfter(10)
